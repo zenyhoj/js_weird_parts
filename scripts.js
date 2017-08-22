c0cfb4e12adb7bb92e42b6f92cf321b1ac9dd88f
zenyhoj <joe.balingit@gmail.com>
2017-08-22 15:59:13 +0800

    Objects and Object literals

--- scripts.js
+++ scripts.js
@@ -7,14 +7,36 @@ var joe = {
     }
 };
 
-//function to accept an object as an argument
-function greet(firstname,  lastname){
-    console.log(joe.firstName + ' ' + joe.lastName);
+
+var eddie = { 
+    firstName: 'eddie', 
+    lastName: 'apatan', 
+    address: {
+        street: 'purok nangka',
+        barangay: 'Poblacion 4'
+        }
+    };
+
+
+//accepts joe object 
+//accepts object created on the fly
+function greet(person){
+    console.log(person.firstName + ' ' + person.lastName + ' ' + person.address.street + ' ' + person.address.barangay);
 
 }
 
+
 greet(joe);//property firstName from Object joe is pass as a value to a function greet();
-greet({firstname: 'eddie', lastname: 'apatan'}); //creates property on the fly and pass it to a function greet();
+greet(eddie);
+// greet({ 
+//     firstName: 'eddie', 
+//     lastName: 'apatan', 
+//     address: {
+//         street: 'purok nangka',
+//         barangay: 'Poblacion 4'
+//     }
+// }); //creates property on the fly and pass it to a function greet();
+
 
 
     
