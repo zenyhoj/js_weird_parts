e21f76bfc945fe8989ed8c80de75e5f5466cc7c2
zenyhoj <joe.balingit@gmail.com>
2017-08-30 16:20:52 +0800

    added script

--- scripts.js
+++ scripts.js
@@ -13,3 +13,7 @@ console.log(arr);
 //invoking a function within an array an passing the name property to a function as a parameter
 arr[3](arr[2].name);
 
+var arrayOne = ['brown', 'fox'];
+var arrayTwo = ['the', 'quick', ...arrayOne];//...spread operator, used to insert a value of an array to another array element
+console.log(arrayTwo);
+
