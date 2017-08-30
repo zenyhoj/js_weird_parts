938cb8873faf0414534143853f274a478166ff94
zenyhoj <joe.balingit@gmail.com>
2017-08-30 15:07:30 +0800

    deleted some files

--- lib1.js
+++
@@ -1,1 +0,0 @@
-var libName = "lib 1";
--- scripts.js
+++ scripts.js
@@ -1,43 +1,12 @@
-var joe = {
-    firstName: 'joe',
-    lastName: 'balingit',
-    address: {
-        street: 'purok 6',
-        barangay: 'rizal'
-    }
-};
-
-
-var eddie = { 
-    firstName: 'eddie', 
-    lastName: 'apatan', 
-    address: {
-        street: 'purok nangka',
-        barangay: 'Poblacion 4'
-        }
-    };
-
-
-//accepts joe object 
-//accepts object created on the fly
-function greet(person){
-    console.log(person.firstName + ' ' + person.lastName + ' ' + person.address.street + ' ' + person.address.barangay);
-
-}
-
-
-greet(joe);//property firstName from Object joe is pass as a value to a function greet();
-greet(eddie);
-// greet({ 
-//     firstName: 'eddie', 
-//     lastName: 'apatan', 
-//     address: {
-//         street: 'purok nangka',
-//         barangay: 'Poblacion 4'
-//     }
-// }); //creates property on the fly and pass it to a function greet();
-
-
-
-
-
+var arr = [
+    1,
+    false,
+    {
+        name: 'joe'
+    },
+    function getName(){
+        console.log('some name');
+    },
+    "string"
+];
+console.log(arr);
