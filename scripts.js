0f28b43023d813f9a3d4771148dc4aa58afbae06
zenyhoj <joe.balingit@gmail.com>
2017-08-30 15:17:52 +0800

    some array scripts

--- scripts.js
+++ scripts.js
@@ -4,9 +4,12 @@ var arr = [
     {
         name: 'joe'
     },
-    function getName(){
-        console.log('some name');
+    function(name){
+        console.log('Hello ' + name);
     },
     "string"
 ];
 console.log(arr);
+//invoking a function within an array an passing the name property to a function as a parameter
+arr[3](arr[2].name);
+
