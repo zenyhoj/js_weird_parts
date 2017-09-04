6cc754904d308f56e945fb7ee93731f5a48e973e
zenyhoj <joe.balingit@gmail.com>
2017-09-04 10:35:44 +0800

    modifying global value or objects within a working function using IIFE

--- scripts.js
+++ scripts.js
@@ -5,3 +5,21 @@ var greetFunc = function(name){
 console.log(greetFunc);//outputs the variable and return and object.
 
 
+(function(firstname){ //also an IIFE. This becomes a function expression by wrapping function with ()
+    var greeting = "hello ";
+    console.log(greeting + firstname);
+}('joe'));
+
+console.log(greeting);//refers to global execution context. outside of the function expression. could be referring to other script
+
+//It's also possible to override global value/object within a function
+(function(global, firstname){ //also an IIFE. This becomes a function expression by wrapping function with ()
+    var greeting = "hello ";
+    global.greeting = "hello "
+    console.log(greeting + firstname);
+    console.log(global.greeting + firstname);
+}(window, 'joe'));
+
+console.log(greeting);//refers to global execution context. outside of the function expression. could be referring to other script
+//value of greeting which is supposed to be hola will be changed to hello -> 
+//
