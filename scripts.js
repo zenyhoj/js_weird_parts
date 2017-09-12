d08e7b4a6a251e97862edf431206f7cefdbee342
zenyhoj <joe.balingit@gmail.com>
2017-09-12 11:51:03 +0800

    added scripts

--- scripts.js
+++ scripts.js
@@ -1,14 +1,4 @@
-var arr = [
-    1,
-    false,
-    {
-        name: 'joe'
-    },
-    function(name){
-        console.log('Hello ' + name);
-    },
-    "string"
-];
+var arr = [1,false,{ name: 'joe'}, function(name) { console.log('Hello ' + name); },"string"]; //function could contain one of the following values
 console.log(arr);
 //invoking a function within an array an passing the name property to a function as a parameter
 arr[3](arr[2].name);
@@ -24,4 +14,27 @@ function addNumbers(one, two, three){
 var numbers = [1,2,31];
 addNumbers(...numbers);
 
+//looping through an array
+var Person = function (name, age, group = "default") {
+    this.name = name,
+        this.age = age,
+        this.group = group
+}
 
+var ArrPerson = [];
+debugger;
+ArrPerson.push(new Person("john", 12, "M1"));
+ArrPerson.push(new Person("sam", 2, "M0"));
+ArrPerson.push(new Person("joe", 33, )); //if group is empty, outputs default
+
+for (var key in ArrPerson) {
+    // if(ArrPerson[key].age === 12){
+    //do something
+    // debugger;
+    // if(ArrPerson[key].group===""){
+
+    console.log(ArrPerson[key].name + '|' + ArrPerson[key].age + '|' + ArrPerson[key].group);
+    // console.log(ArrPerson[key].age);
+    // console.log(ArrPerson[key].group);
+    //}
+}
